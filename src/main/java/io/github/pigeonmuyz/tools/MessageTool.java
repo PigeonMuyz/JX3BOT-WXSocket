63a778bf3f6ae2b4f8e7c2fab2d79d7a3ad75b76
PigenMuyz <45222425+PigeonMuyz@users.noreply.github.com>
2024-03-04 16:22:42 +0800

    Update MessageTool.java

--- src/main/java/io/github/pigeonmuyz/tools/MessageTool.java
+++ src/main/java/io/github/pigeonmuyz/tools/MessageTool.java
@@ -20,19 +20,19 @@ public class MessageTool {
 static String temp;
     public static MessageType singleCommand(String command, String userID, String channelID, String server) {
         try {
-            if (channelID != null){
-                rootNode = mapper.readTree(HttpTool.getData("http://localhost:25555/user/get?GroupID="+channelID));
-                if (!rootNode.get("data").isEmpty()){
-                    dataNode = rootNode.get("data");
-                    server = dataNode.get("server").asText();
-                }
-            }else{
-                rootNode = mapper.readTree(HttpTool.getData("http://localhost:25555/user/get?WXID="+userID));
-                if (!rootNode.get("data").isEmpty()){
-                    dataNode = rootNode.get("data");
-                    server = dataNode.get("server").asText();
-                }
-            }
+            //if (channelID != null){
+            //    rootNode = mapper.readTree(HttpTool.getData("http://localhost:25555/user/get?GroupID="+channelID));
+            //    if (!rootNode.get("data").isEmpty()){
+            //        dataNode = rootNode.get("data");
+            //        server = dataNode.get("server").asText();
+            //    }
+            //}else{
+            //    rootNode = mapper.readTree(HttpTool.getData("http://localhost:25555/user/get?WXID="+userID));
+            //    if (!rootNode.get("data").isEmpty()){
+            //        dataNode = rootNode.get("data");
+            //        server = dataNode.get("server").asText();
+            //    }
+            //}
             switch(command){
                 //region 日常
                 case "日常":
