2da33eb60a3b1aa17be0ec57536fef3f948c0d00
htc <muyz0319@icloud.com>
2024-03-23 09:00:21 +0800

    删除漂流瓶功能

--- src/main/java/io/github/pigeonmuyz/Main.java
+++ src/main/java/io/github/pigeonmuyz/Main.java
@@ -3,7 +3,6 @@ package io.github.pigeonmuyz;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.pigeonmuyz.entity.MessObject;
-import io.github.pigeonmuyz.entity.MessageBottle;
 import io.github.pigeonmuyz.helper.MessFilter;
 import io.github.pigeonmuyz.tools.HttpTool;
 import io.github.pigeonmuyz.tools.ManualTimer;
@@ -11,7 +10,6 @@ import io.github.pigeonmuyz.websocket.SocketClient;
 import io.github.pigeonmuyz.websocket.SocketServer;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.message.Message;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,13 +17,11 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final Logger log = LogManager.getLogger(Main.class);
     public static Properties configProperties = new Properties();
     public static List<MessObject> personal;
-    public static List<MessageBottle> messsageBottles;
     private static final String JSON_FILE_PATH = "./personal.json";
     private static final String MESSAGE_BOTTLE_FILE_PATH = "./messageBottle.json";
     private static final ObjectMapper mapper = new ObjectMapper();
@@ -36,17 +32,11 @@ public class Main {
             configProperties.load(in);
             HttpTool.getComputerId();
             personal = readMessObjectFromJsonFile(JSON_FILE_PATH);
-            messsageBottles = readMessageBottleFromJsonFile(MESSAGE_BOTTLE_FILE_PATH);
 
             //计时器1
             Runnable task1 = () -> writeToMessObjectJsonFile(JSON_FILE_PATH, personal);
             ManualTimer timer1 = new ManualTimer(task1, JSON_FILE_PATH);
             timer1.start(20, 60);
-
-            //计时器1
-            Runnable task2 = () -> writeToMessageBottleJsonFile(MESSAGE_BOTTLE_FILE_PATH, messsageBottles);
-            ManualTimer timer2 = new ManualTimer(task2, MESSAGE_BOTTLE_FILE_PATH);
-            timer2.start(20, 60);
 
             // WS服务端
             SocketServer s = new SocketServer(Integer.parseInt(configProperties.getProperty("config.wssport")));
@@ -76,29 +66,6 @@ public class Main {
     }
 
     private static void writeToMessObjectJsonFile(String filePath, List<MessObject> data) {
-        try {
-            mapper.writeValue(new File(filePath), data);
-        } catch (IOException e) {
-            log.error("配置文件写入失败");
-            e.printStackTrace();
-        }
-    }
-
-    private static List<MessageBottle> readMessageBottleFromJsonFile(String filePath) {
-        File file = new File(filePath);
-        if (file.exists()) {
-            try {
-                log.info("MessageBottle读取成功");
-                return mapper.readValue(file, new TypeReference<List<MessageBottle>>() {});
-            } catch (IOException e) {
-                log.error("配置文件读取失败");
-                e.printStackTrace();
-            }
-        }
-        return new ArrayList<MessageBottle>();
-    }
-
-    private static void writeToMessageBottleJsonFile(String filePath, List<MessageBottle> data) {
         try {
             mapper.writeValue(new File(filePath), data);
         } catch (IOException e) {
